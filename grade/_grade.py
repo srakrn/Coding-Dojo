8fa3f36cf5a986c678596549bafec1f8870b73f0
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-03-25 02:22:44 +0700

    Initialising class

--- grade/_grade.py
+++ grade/_grade.py
@@ -1,3 +1,5 @@
 class Record:
-    def __init__(self):
+    def __init__(self, score, name=None):
+        self.score = score
+        self.name = name
         pass
--- test.py
+++ test.py
@@ -2,5 +2,14 @@ import unittest
 
 import grade
 
+
+class RecordTest(unittest.TestCase):
+    def setUp(self):
+        self.record_a = grade.Record(73, name="Computers and Programming")
+
+    def test_class_type(self):
+        self.assertIsInstance(self.record_a, grade.Record)
+
+
 if __name__ == "__main__":
     unittest.main()
