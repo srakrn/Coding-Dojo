6d7c54f0cf1de15bdededfe323ce8521ea8ebfab
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-03-25 02:38:48 +0700

    Changing grade

--- grade/_grade.py
+++ grade/_grade.py
@@ -9,16 +9,27 @@ def pair(it):
 
 class Record:
     def __init__(self, score, name=None):
-        self.score = score
         self.name = name
-        self.calculate_grade()
+        self.set_score(score)
         pass
 
-    def calculate_grade(self):
+    def set_score(self, score):
+        self._score = score
+        self._calculate_grade()
+
+    def _calculate_grade(self):
         ranges = [100, 80, 75, 70, 65, 60, 55, 50, 0]
         grades = [4, 3.5, 3, 2.5, 2, 1.5, 1, 0]
         for (upper_bound, lower_bound), grade in zip(pair(ranges), grades):
             if self.score >= lower_bound and self.score < upper_bound:
-                self.grade = grade
+                self._grade = grade
                 return
         raise IndexError("Score not in range.")
+
+    @property
+    def score(self):
+        return self._score
+
+    @property
+    def grade(self):
+        return self._grade
--- test.py
+++ test.py
@@ -12,14 +12,18 @@ class RecordTest(unittest.TestCase):
 
     def test_grade(self):
         self.assertEqual(self.record_a.grade, 3)
-    
+
     def test_unknown_grade(self):
         try:
             self.record_b = grade.Record(-1, name="Some Erronous Subject")
         except IndexError:
             pass
         else:
-            self.fail('IndexError not raised')
+            self.fail("IndexError not raised")
+
+    def test_grade_change(self):
+        self.record_a.set_score(20)
+        self.assertEqual(self.record_a.grade, 0)
 
 
 if __name__ == "__main__":
